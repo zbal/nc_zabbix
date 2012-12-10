2f13b8e8ae08db8029134457fae4ad35c5750aa4
Vincent Viallet <vincent@wiredcraft.com>
2012-12-11 01:26:22 +0800

    Change async functions..

--- js/app.js
+++ js/app.js
@@ -98,38 +98,29 @@
             // waterfall execution
             async.waterfall([
                 function(cb) {
-                    // check if application exists for hostid
-                    $('#results').append('Checking for existing application: ... ');
-                    window.zabbix.call('application.exists', {
-                        hostid: $('#hosts').val(),
-                        name: 'Distributed Web Monitoring'
+                    $('#results').append('Fetching application id: ... ');
+                    window.zabbix.call('application.get', {
+                        hostids: $('#hosts').val(),
+                        filter: {name: 'Distributed Web Monitoring'},
+                        output: 'shorten'
                     }, function(err, resp) {
                         if (err) {
                             cb(err);
                         } else {
-                            $('#results').append(resp.result ? 'exist<br/>' : 'not existing</br>')
-                            cb(null, resp.result)
+                            if (_.isEmpty(resp.result)) {
+                                $('#results').append('not existing.</br>');
+                                cb()
+                            } else {
+                                $('#results').append('done</br>');
+                                cb(null, resp.result[0].applicationid);
+                            }
                         }
                     });
                 },
-                function(exists, cb) {
-                    if (exists) {
-                        // get application id
-                        $('#results').append('Fetching application id: ... ');
-                        window.zabbix.call('application.get', {
-                            hostids: $('#hosts').val(),
-                            filter: {name: 'Distributed Web Monitoring'},
-                            output: 'shorten'
-                        }, function(err, resp) {
-                            if (err) {
-                                cb(err);
-                            } else {
-                                $('#results').append('done</br>');
-                                cb(null, resp.result[0].applicationid)
-                            }
-                        });
+                function(id, cb) {
+                    if (id) {
+                        cb(null, id);
                     } else {
-                        // create application and fetch id
                         $('#results').append('Creating new application: ... ');
                         window.zabbix.call('application.create', {
                             hostid: $('#hosts').val(),
@@ -149,6 +140,58 @@
                     $('#results').append('That\'s enough for today ! going back home...</br>');
                     cb(null);
                 }
+                // function(cb) {
+                //     // check if application exists for hostid
+                //     $('#results').append('Checking for existing application: ... ');
+                //     window.zabbix.call('application.exists', {
+                //         hostid: $('#hosts').val(),
+                //         name: 'Distributed Web Monitoring'
+                //     }, function(err, resp) {
+                //         if (err) {
+                //             cb(err);
+                //         } else {
+                //             $('#results').append(resp.result ? 'exist<br/>' : 'not existing</br>')
+                //             cb(null, resp.result)
+                //         }
+                //     });
+                // },
+                // function(exists, cb) {
+                //     if (exists) {
+                //         // get application id
+                //         $('#results').append('Fetching application id: ... ');
+                //         window.zabbix.call('application.get', {
+                //             hostids: $('#hosts').val(),
+                //             filter: {name: 'Distributed Web Monitoring'},
+                //             output: 'shorten'
+                //         }, function(err, resp) {
+                //             if (err) {
+                //                 cb(err);
+                //             } else {
+                //                 $('#results').append('done</br>');
+                //                 cb(null, resp.result[0].applicationid)
+                //             }
+                //         });
+                //     } else {
+                //         // create application and fetch id
+                //         $('#results').append('Creating new application: ... ');
+                //         window.zabbix.call('application.create', {
+                //             hostid: $('#hosts').val(),
+                //             name: 'Distributed Web Monitoring'
+                //         }, function(err, resp) {
+                //             if (err) {
+                //                 cb(err);
+                //             } else {
+                //                 $('#results').append('done</br>');
+                //                 cb(null, resp.result.applicationids)
+                //             }
+                //         });
+                //     }
+                // },
+                // function(appid, cb) {
+                //     $('#results').append('application id: '+ appid +'...</br>');
+                //     $('#results').append('That\'s enough for today ! going back home...</br>');
+                //     cb(null);
+                // }
             ], function(err, result) {
                 if (err) {
                     $('#error').html(err.data ? err.data : err.message);
