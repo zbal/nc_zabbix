09b133107d0c70441f3f282131c5ddfcb6198a54
Vincent Viallet <vincent@wiredcraft.com>
2013-01-07 16:22:39 +0800

    Update zabbix helper, get haproxy create... part1

--- js/views/haproxy.js
+++ js/views/haproxy.js
@@ -14,33 +14,72 @@
             this.$el.html(templates.haProxyAdd());
         },
         processHaProxy: function() {
+            var view = this;
+            var host = view.host;
             var csv = $('#csv').val();
             var obj = $.csv.toObjects(csv);
-            var keys = {};
+            var items = [];
             
             _.each(obj, function(server) {
-                keys[server['# pxname'] +' '+ server['svname']] = [];
-                var param = [
-                    'hrsp_5xx',
-                    'chkdown',
-                    'chkfail',
-                    'econ',
-                    'eresp',
-                    'check_status',
-                    'name',
-                    'scur',
-                    'slim',
-                    'smax',
-                    'rate',
-                    'rate_max',
-                    'status'
+                var items = [
+                    { name: 'hrsp_5xx',     description: 'http_5xx responses',  delay: 1800, value_type: 0 },
+                    { name: 'chkdown',      description: 'Check down',          delay: 1800, value_type: 0 },
+                    { name: 'chkfail',      description: 'Check fail',          delay: 1800, value_type: 0 },
+                    { name: 'econ',         description: 'Errors Connection',   delay: 1800, value_type: 0 },
+                    { name: 'eresp',        description: 'Errors Response',     delay: 1800, value_type: 0 },
+                    { name: 'check_status', description: 'Last Check Status',   delay: 1800, value_type: 1 },
+                    { name: 'name',         description: 'Name',                delay: 1800, value_type: 1 },
+                    { name: 'scur',         description: 'Session Current',     delay: 300,  value_type: 0 },
+                    { name: 'slim',         description: 'Session Limit',       delay: 1800, value_type: 0 },
+                    { name: 'smax',         description: 'Session Max',         delay: 1800, value_type: 0 },
+                    { name: 'rate',         description: 'Session Rate Current', delay: 300, value_type: 0 },
+                    { name: 'rate_max',     description: 'Session Rate Max',    delay: 1800, value_type: 0 },
+                    { name: 'status',       description: 'Status',              delay: 300,  value_type: 1 },
                 ]
-                param.forEach(function(key) {
-                    var key_ = 'haproxy[stat,'+ key +','+ server['# pxname'] +','+ server['svname'] +']';
-                    keys[server['# pxname'] +' '+ server['svname']].push(key_);
+
+                items.forEach(function(item) {
+                    var localItem = _.clone(item);
+
+                    // common data
+                    localItem.history = 15;
+                    localItem.trends = 60;
+                    localItem.hostid = host.hostid;
+
+                    localItem.key_ = 'haproxy[stat,'+ item.name +','+ server['# pxname'] +','+ server['svname'] +']';
+
+                    // do some filtering as not all the items apply to all type of srvname
+                    switch (item.name) {
+                        case 'slim':
+                            if (server.svname !== 'BACKEND') {
+                                // session limit only or frontend and regular servers
+                                items.push(localItem);
+                            }
+                            break;
+                        default:
+                            items.push(localItem);
+                    }
                 });
             });
-            console.log(keys);
+            
+            window.zabbix.getApplicationId('HaProxy', {
+                hostid: view.hostid
+            }, function(err, applicationId) {
+                if (err) return;
+                if (!applicationId) return;
+                items.forEach(function(item) {
+                    console.log('processing item');
+                    item.applications = [ applicationId ];
+                    
+                    window.zabbix.saveItem(item, function(err, itemId) {
+                        if (err) {
+                            console.log(err)
+                            return
+                        }
+                        console.log('successs ! '+ itemId);
+                    });
+                });
+            })
+            console.log(items);
         },
         fetchHaProxy: function() {
             var url = $('#url').val();
--- js/zabbix.js
+++ js/zabbix.js
@@ -62,4 +62,103 @@ Client.prototype.authenticate = function authenticate(callback) {
     });
 };
 
+// Helpers
+
+// Get a single application id, based on the name, for a host
+Client.prototype.getApplicationId = function(name, options, callback) {
+    var self = this;
+    
+    // required
+    if (!name) return callback(new Error('Missing application name'));
+    if (!options.hostid) return callback(new Error('Missing hostid'));
+    
+    // by default create if not existing
+    var create = true;
+    var hostid = options.hostid;
+
+    self.call('application.get', {
+        hostids: [ hostid ],
+        filter: {name: name},
+        output: 'shorten'
+    }, function(err, resp) {
+        if (err) return callback(err);
+        if (_.isEmpty(resp.results)) {
+            if (create) {
+                self.call('application.create', {
+                    hostid: hostid,
+                    name: name
+                }, function(err, resp) {
+                    if (err) {
+                        callback(err);
+                    } else {
+                        callback(null, resp.result.applicationids[0]);
+                    }
+                });
+            } else {
+                callback(null, false)
+            }
+        } else {
+            callback(null, resp.result[0].applicationid)
+        }
+    }
+}
+
+// Save item
+// Either create or update an item
+// require hostid + application + item options
+Client.prototype.saveItem = function(item, options, callback) {
+    var self = this;
+    
+    // required fields for item
+    if (!item) return callback(new Error('Missing item'));
+    if (!item.key_) return callback(new Error('Missing item key_'));
+    if (!item.description) return callback(new Error('Missing item description'));
+    if (!item.type) item.type = 0; // zabbix_get
+    if (!item.value_type) return callback(new Error('Missing item value_type'));
+    if (!item.status) item.status = 0; // enable item
+    if (!item.history) return callback(new Error('Missing item history'));
+    if (!item.trends) return callback(new Error('Missing item trends'));
+    if (!item.hostid) return callback(new Error('Missing item hostid'));
+    if (!item.delay) return callback(new Error('Missing item delay'));
+    if (!item.applications) return callback(new Error('Missing item applications'));
+
+    var create = true,
+        update = true;
+
+    self.call('item.get', {
+        hostids: [ item.hostid ],
+        filter: {key_: item.key_},
+        output: 'shorten'
+    }, function(err, resp) {
+        if (err) {
+            callback(err);
+        } else {
+            if (_.isEmpty(resp.result)) {
+                // not existing - simply create
+                self.call('item.create', item, function(err, resp) {
+                    if (err) {
+                        callback(err);
+                    } else {
+                        callback(null, resp.result.itemids[0]);
+                    }
+                });
+            } else {
+                // already existing - choose to update or do nothing...
+                if (update) {
+                    item.itemid = resp.result[0].itemid;
+                    self.call('item.update', item, function(err, resp) {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback(null, resp.result.itemids[0]);
+                        }
+                    });
+                } else {
+                    callback(null, resp.result[0].itemid);
+                }
+            }
+        }
+    });
+}
+
 window.Zabbix = Client;
